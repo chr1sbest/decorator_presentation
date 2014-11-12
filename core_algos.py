a91cfb91b46eb7327abd45aae1902b31b2939c57
Chris Best <chr2sbest@gmail.com>
2014-11-12 11:25:52 -0800

    removed decorator stuff from core_algos

--- core_algos.py
+++ core_algos.py
@@ -1,16 +1,11 @@
-from decorator_example import timer
-
-@timer
 def some_calculation(value):
     """Add one to a value."""
     return value + 1
 
-@timer
 def some_other_calculation(value, factor):
     """Multiply a value by a factor."""
     return value * factor
 
-@timer
 def the_final_calculation(value, increment_by_one=False):
     """Double a value and optionally increment by 1."""
     return (value * 2) + 1 if increment_by_one else value * 2
