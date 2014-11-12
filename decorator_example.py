881a7f5b22390b2bd9f92641fb5e9ac75700cd2a
Chris Best <chr2sbest@gmail.com>
2014-11-12 15:53:06 -0800

    moved decorator comments to top

--- decorator_example.py
+++ decorator_example.py
@@ -1,6 +1,9 @@
 from functools import wraps
 from time import time as now
 
+# If func not decorated,          func() runs as -> func()
+# If func decorated with @timer,  func() runs as -> timer(func())
+
 def timer(func):    # Takes decorated function as the sole parameter.
     @wraps(func)    # Passes decorated function's metadata to wrapper.
     def wrapper(*args, **kwargs):
@@ -12,6 +15,3 @@ def timer(func):    # Takes decorated function as the sole parameter.
         print "Execution time was {0}ms".format(exec_time)
         return result
     return wrapper
-
-# If func not decorated,          func() runs as -> func()
-# If func decorated with @timer,  func() runs as -> timer(func())
